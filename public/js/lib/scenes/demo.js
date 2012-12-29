c67b24c4cf1a508b5d77668c5d7b98db29d4cc86
mozair <macskeptic@gmail.com>
2012-12-28 22:47:57 -0200

    reformatting for readability

--- public/js/lib/scenes/demo.js
+++ public/js/lib/scenes/demo.js
@@ -7,34 +7,10 @@ define([
       player({ x: 50, y: 300 }),
       player({ x: 1100, y: 350 }),
       player({ x: 500, y: 50 }),
-      {
-        x: 300,
-        y: 000,
-        width: 100,
-        height: 100,
-        type: 'square'
-      },
-      {
-        x: 200,
-        y: 550,
-        width: 100,
-        height: 100,
-        type: 'square'
-      },
-      {
-        x: 800,
-        y: 100,
-        width: 100,
-        height: 100,
-        type: 'square'
-      },
-      {
-        x: 800,
-        y: 500,
-        width: 100,
-        height: 100,
-        type: 'square'
-      },
+      { x: 300, y: 000, width: 100, height: 100, type: 'square' },
+      { x: 200, y: 550, width: 100, height: 100, type: 'square' },
+      { x: 800, y: 100, width: 100, height: 100, type: 'square' },
+      { x: 800, y: 500, width: 100, height: 100, type: 'square' },
       boundaries()
     ];
 
@@ -51,45 +27,45 @@ define([
       }
 
       if(input.keyPressed('a')){
-        entities[0].velocityX=-1;
-        entities[0].velocityY=0;
+        entities[0].velocityX = -1;
+        entities[0].velocityY = 0;
       }
       if(input.keyPressed('s')){
-        entities[0].velocityX=0;
-        entities[0].velocityY=1;
+        entities[0].velocityX = 0;
+        entities[0].velocityY = 1;
       }
       if(input.keyPressed('d')){
-        entities[0].velocityX=1;
-        entities[0].velocityY=0;
+        entities[0].velocityX = 1;
+        entities[0].velocityY = 0;
       }
       if(input.keyPressed('w')){
-        entities[0].velocityX=0;
-        entities[0].velocityY=-1;
+        entities[0].velocityX = 0;
+        entities[0].velocityY = -1;
       }
       if(input.keyPressed('q')){
-        entities[0].velocityX=0;
-        entities[0].velocityY=0;
+        entities[0].velocityX = 0;
+        entities[0].velocityY = 0;
       }
 
       if(input.keyPressed('left')){
-        entities[1].velocityX=-1;
-        entities[1].velocityY=0;
+        entities[1].velocityX = -1;
+        entities[1].velocityY = 0;
       }
       if(input.keyPressed('down')){
-        entities[1].velocityX=0;
-        entities[1].velocityY=1;
+        entities[1].velocityX = 0;
+        entities[1].velocityY = 1;
       }
       if(input.keyPressed('right')){
-        entities[1].velocityX=1;
-        entities[1].velocityY=0;
+        entities[1].velocityX = 1;
+        entities[1].velocityY = 0;
       }
       if(input.keyPressed('up')){
-        entities[1].velocityX=0;
-        entities[1].velocityY=-1;
+        entities[1].velocityX = 0;
+        entities[1].velocityY = -1;
       }
       if(input.keyPressed('forwardslash')){
-        entities[1].velocityX=0;
-        entities[1].velocityY=0;
+        entities[1].velocityX = 0;
+        entities[1].velocityY = 0;
       }
       
     }
