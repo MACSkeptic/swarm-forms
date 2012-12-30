f0dc83b95df322bd919e36b4d6926f015c03df71
mozair <macskeptic@gmail.com>
2012-12-30 17:11:15 -0200

    little fixes to geometry.js

--- public/js/lib/utils/geometry.js
+++ public/js/lib/utils/geometry.js
@@ -1,18 +1,15 @@
 define(function () {    
 
-  function createVector2dFromPointAndModule(x,y,module) {
+  function createVector2dFromPointAndModule(x, y, module) {
     var speedVector = $V([module, 0, 0]);
     var destinationVector = $V([x, y, 0]);
-
     var normalLine = Line.create([0, 0, 0], [0, 0, 1]);
     var angle = speedVector.angleFrom(destinationVector);
     var turnDirection = speedVector.cross(destinationVector);
-    if (turnDirection.elements[2] < 0) {
-      angle = Math.PI*2 - angle;
-    }
+    if (turnDirection.elements[2] < 0) { angle = Math.PI*2 - angle; }
     var rotatedSpeed = speedVector.rotate(angle, normalLine);  
     return { x: rotatedSpeed.elements[0], y: rotatedSpeed.elements[1] };
   }
 
-  return { createVector2dFromPointAndModule = createVector2dFromPointAndModule; }
+  return { createVector2dFromPointAndModule: createVector2dFromPointAndModule; }
 }
