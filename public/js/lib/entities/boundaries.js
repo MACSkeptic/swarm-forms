16c68236d2cc1968adbaa8db7fea0f22a3d72d7e
mozair <macskeptic@gmail.com>
2013-01-01 17:03:17 -0200

    fix boundaries

--- public/js/lib/entities/boundaries.js
+++ public/js/lib/entities/boundaries.js
@@ -1,7 +1,7 @@
 define(function () {
 
   function create(specs) {
-    return _.extend({ minX: 0, maxX: 1280, minY: 0, maxY: 720 }, specs || {});
+    return _.extend({ type: 'boundaries', minX: 0, maxX: 1280, minY: 0, maxY: 720 }, specs || {});
   }
 
   return create;
