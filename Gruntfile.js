0e9c65847a8b631996f6b56c5a5f1cf739a25159
mozair <macskeptic@gmail.com>
2013-01-01 15:01:16 -0200

    started adding jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,14 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     qunit: { all: ['http://localhost:19999/js/tests/all.html'] },
-    connect: { server: { options: { port: 19999, base: './public' } } }
+    connect: { server: { options: { port: 19999, base: './public' } } },
+    jshint: { all: ['Gruntfile.js'] }
   });
 
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.registerTask('test', ['connect', 'qunit']);
   grunt.registerTask('default', ['test']);
-}
+};
