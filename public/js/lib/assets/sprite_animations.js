0ba9024d2c55f4868defcc75144e8fc07ecde203
Romulo Santos <romulobr@gmail.com>
2013-01-10 01:26:07 -0200

    improved sprite code, prepared sprites for actor.

--- public/js/lib/assets/sprite_animations.js
+++ public/js/lib/assets/sprite_animations.js
@@ -10,17 +10,64 @@ define(
     flow: 'loop'
   },
 
-  linkRunningRight: {
+  linkWalkingRight: {
     frames: {
-      'linkRunningRight1': 50,
-      'linkRunningRight2': 50,
-      'linkRunningRight3': 50,
-      'linkRunningRight4': 50,
-      'linkRunningRight5': 50,
-      'linkRunningRight6': 50,
-      'linkRunningRight7': 50
+      'linkWalkingRight1': 100,
+      'linkWalkingRight2': 100,
+      'linkWalkingRight3': 100,
+      'linkWalkingRight4': 100,
+      'linkWalkingRight5': 100,
+      'linkWalkingRight6': 100,
+      'linkWalkingRight7': 100,
+      'linkWalkingRight8': 100,
+      'linkWalkingRight9': 100
     },
-    flow: 'reverse-loop'
+    flow: 'loop'
+  },
+
+  linkWalkingLeft: {
+    frames: {
+      'linkWalkingLeft1': 100,
+      'linkWalkingLeft2': 100,
+      'linkWalkingLeft3': 100,
+      'linkWalkingLeft4': 100,
+      'linkWalkingLeft5': 100,
+      'linkWalkingLeft6': 100,
+      'linkWalkingLeft7': 100,
+      'linkWalkingLeft8': 100,
+      'linkWalkingLeft9': 100
+    },
+    flow: 'loop'
+  },
+
+  linkWalkingDown: {
+    frames: {
+      'linkWalkingDown1': 100,
+      'linkWalkingDown2': 100,
+      'linkWalkingDown3': 100,
+      'linkWalkingDown4': 100,
+      'linkWalkingDown5': 100,
+      'linkWalkingDown6': 100,
+      'linkWalkingDown7': 100,
+      'linkWalkingDown8': 100,
+      'linkWalkingDown9': 100
+    },
+    flow: 'loop'
+  },
+
+  linkWalkingUp: {
+    frames: {
+      'linkWalkingUp1': 100,
+      'linkWalkingUp2': 100,
+      'linkWalkingUp3': 100,
+      'linkWalkingUp4': 100,
+      'linkWalkingUp5': 100,
+      'linkWalkingUp6': 100,
+      'linkWalkingUp7': 100,
+      'linkWalkingUp8': 100,
+      'linkWalkingUp9': 100
+    },
+    flow: 'loop'
   }
 
 });
--- public/js/lib/scenes/sprites_room.js
+++ public/js/lib/scenes/sprites_room.js
@@ -2,13 +2,25 @@ define(function (require) {
   var spriteBasedEntity  = require('../entities/sprite_based_entity'),
       spriteAnimation = require('../assets/animated_sprite'),
       spriteAnimations = require('../assets/sprite_animations'),
+      spriteActor = require('../assets/sprite_actor'),
       entities = [];
 
   var zero = spriteBasedEntity({ sprite: 'zeroStanding1' });
-  var link = spriteBasedEntity({ sprite: 'linkRunningRight' });
+  var link = spriteBasedEntity({ sprite: 'linkWalkingRight' });
   
   var zeroStandingAnimation = spriteAnimation(spriteAnimations.zeroStanding);
-  var linkRunningUpAnimation = spriteAnimation(spriteAnimations.linkRunningRight);
+  var linkWalkingUpAnimation = spriteAnimation(spriteAnimations.linkWalkingUp);
+  var linkWalkingDownAnimation = spriteAnimation(spriteAnimations.linkWalkingDown);
+  var linkWalkingLeftAnimation = spriteAnimation(spriteAnimations.linkWalkingLeft);
+  var linkWalkingRightAnimation = spriteAnimation(spriteAnimations.linkWalkingRight);
+
+  var linkActor = spriteActor({states: {
+    up: linkWalkingUpAnimation,
+    down: linkWalkingDownAnimation,
+    left: linkWalkingLeftAnimation,
+    right: linkWalkingRightAnimation
+  }
+  });
 
   function init(callback) {
     entities.push(zero);
@@ -20,9 +32,9 @@ define(function (require) {
 
   function update(params) {
     zeroStandingAnimation.update(params.elapsed);
-    linkRunningUpAnimation.update(params.elapsed);
+    linkWalkingUpAnimation.update(params.elapsed);
     
-    link.sprite = linkRunningUpAnimation.currentSprite();
+    link.sprite = linkWalkingUpAnimation.currentSprite();
     zero.sprite = zeroStandingAnimation.currentSprite();
   }
 
