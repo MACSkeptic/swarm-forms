6bc73c904299306d03d18b9dca5bd3bc0b385db4
mozair <macskeptic@gmail.com>
2013-01-20 13:15:42 -0200

    fixing file name conventions on guns

--- public/js/lib/components/guns.js
+++ public/js/lib/components/guns.js
@@ -1,4 +1,4 @@
-define(['./guns/basicGun', './guns/chaserCreatorGun'], function (basicGun, chaserCreatorGun) {
+define(['./guns/basic_gun', './guns/chaser_creator_gun'], function (basicGun, chaserCreatorGun) {
   var guns = {};
   guns.basic = basicGun;
   guns.chaserCreator = chaserCreatorGun;
